919c0d9462257d7d9b82b3b30d1081502f937510
cihanbagriyanik <cihanbagriyanikde@gmail.com>
2024-01-23 12:22:25 +0400

    nodejs Setup and oop1 Start

---
+++ code/nodejs/05-OOP_1/oop1.js
@@ -0,0 +1,20 @@
+"use strict"
+/* ---------------------------------------------
+
+    OOP - Object Oriented  Programs
+
+----------------------------------------------- */
+
+/* --------------------------------------------- 
+    OBJECTS
+-----------------------------------------------*/
+
+const camelCaseNamedObject = {
+
+    propertyName: 'value',  // field, attribute
+
+    methodName: function() {
+        
+    }
+
+}
