70585a40803b70463d40b9bceb449eebaddd5aa8
cihanbagriyanik <cihanbagriyanikde@gmail.com>
2024-01-26 13:57:54 +0400

    nodejs modules 2.class module export import

--- code/nodejs/07-NodeJS_Moduls/index.js
+++ code/nodejs/07-NodeJS_Moduls/index.js
@@ -1,2 +1,35 @@
-"use strict"
+"use strict";
 console.log("hello");
+const { log } = require("console");
+//* require process
+// require('./modules/module.js')
+// require('./modules/module')
+//* if the file name index.js
+// require('./modules/')
+
+//?Single function
+// const test = require('./modules/module')
+// test()
+// console.log(test);
+
+//? multi function
+//! With Array
+// const [test1, test2, test3] = require("./modules/module");
+// test1();
+// test2();
+// test3();
+// test1(), test2(), test3();
+
+//! with Object
+const {
+  testFunctionA,
+  testFunctionB: testB,
+  testFunctionC,
+  pi,
+} = require("./modules/module");
+testFunctionA();
+testB();
+testFunctionC();
+testFunctionA(), testB(), testFunctionC();
+
+console.log(pi);
--- code/nodejs/07-NodeJS_Moduls/modules/module.js
+++ code/nodejs/07-NodeJS_Moduls/modules/module.js
@@ -1,2 +1,67 @@
-"use strict"
+"use strict";
+
 console.log(" module file ");
+
+//? single function
+// const testFunction = function () {
+//   console.log(" test function ");
+// };
+// module.exports = testFunction;
+
+// var x = `adfdgsdrfsdras`
+// module.exports = x
+
+//? multi function
+// const testFunctionA = function () {
+//   console.log(" test function A ");
+// };
+
+// const testFunctionB = function () {
+//   console.log(" test function B ");
+// };
+
+// const testFunctionC = function () {
+//   console.log(" test function C ");
+// };
+
+// const pi = 3.14;
+
+// module.exports = [testFunctionA, testFunctionB, testFunctionC];
+// module.exports = {
+//   testA: testFunctionA,
+//   testFunctionB: testFunctionB,
+//   testFunctionC: testFunctionC,
+//   pi: pi,
+// };
+
+//? short form / hand
+// module.exports.testFunctionA = function () {
+//   console.log(" test function A ");
+// };
+
+// module.exports.testFunctionB = function () {
+//   console.log(" test function B ");
+// };
+
+// module.exports.testFunctionC = function () {
+//   console.log(" test function C ");
+// };
+
+// module.exports.pi = 3.14;
+
+//? 2. Short form / hand
+module.exports = {
+  testFunctionA: function () {
+    console.log(" test function A ");
+  },
+
+  testFunctionB: function () {
+    console.log(" test function B ");
+  },
+
+  testFunctionC: function () {
+    console.log(" test function C ");
+  },
+
+  pi: 3.14,
+};
