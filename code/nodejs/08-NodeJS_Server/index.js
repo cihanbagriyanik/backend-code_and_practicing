cc53ba047b4bc5d040bb4ef4a3de61f5d8781024
cihanbagriyanik <cihanbagriyanikde@gmail.com>
2024-01-30 00:04:40 +0100

    nodeJs Server 2.class end

--- code/nodejs/08-NodeJS_Server/index.js
+++ code/nodejs/08-NodeJS_Server/index.js
@@ -3,10 +3,51 @@ console.log("node server");
 
 const http = require("node:http");
 
+/*
 const app = http.createServer((request, response) => {
   console.log(request);
 
   response.end("welcome first server");
 });
 // app.listen(8000, () => console.log("server runned: http://127.0.0.1:8000"));
+app.listen(8000, () => console.log("server runned: http://localhost:8000"));
+*/
+
+/* -------------------------------------------------------------------------- */
+
+// const app = http.createServer((req, res) => {
+//   if (req.url == `/`) res.end(" <h1> welcome HOME page </h1> ");
+//   else if (req.url == `/login`) res.end(" <h1> welcome LOGIN page </h1> ");
+//   else res.end(" <h1> welcome server running </h1> ");
+// });
+// app.listen(8000, () => console.log("server runned: http://localhost:8000"));
+
+/* -------------------------------------------------------------------------- */
+
+// const app = http.createServer((req, res) => {
+//   if (req.url == `/`) {
+//     res.write(`first result`);
+//     res.write("second result");
+//     res.end(" <h1> welcome HOME page </h1> ");
+//   } else if (req.url == `/login`) {
+//     // res.write(`first result`);
+//     // res.write("second result");
+//     res.end();
+//   } else res.end(" <h1> welcome server running </h1> ");
+// });
+// app.listen(8000, () => console.log("server runned: http://localhost:8000"));
+
+/* -------------------------------------------------------------------------- */
+
+const app = http.createServer((req, res) => {
+  if (req.url == `/`) {
+    if (req.method == `GET`) {
+      res.end(" <h1> welcome HOME page </h1> ");
+    } else if (req.method == `POST`) {
+      res.end(" <h1> POST Meothod </h1> ");
+    }
+  } else if (req.url == `/login`) {
+    res.end();
+  } else res.end(" <h1> welcome server running </h1> ");
+});
 app.listen(8000, () => console.log("server runned: http://localhost:8000"));
