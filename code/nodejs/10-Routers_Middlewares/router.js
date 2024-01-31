c91123b8d043676f1ec0532ad547d20e8729d8c5
cihanbagriyanik <cihanbagriyanikde@gmail.com>
2024-01-31 10:51:32 +0100

    Routers_Middlewares 31.01.2024 middleware start

--- code/nodejs/10-Routers_Middlewares/router.js
+++ code/nodejs/10-Routers_Middlewares/router.js
@@ -9,7 +9,7 @@ const app = express();
 require("dotenv").config();
 const PORT = process.env.PORT || 8000;
 
-/* ------------------------------------------------------- */
+/* -------------------------------------------------------------------------- */
 //? "Router" is special app for URL control in ExpressJS.
 
 // app.get('/', (req, res) => { res.send({ message: 'Home Page' }) })
@@ -24,10 +24,12 @@ const PORT = process.env.PORT || 8000;
 // // Router to App:
 // app.use(router)
 
+/* -------------------------------------------------------------------------- */
 // const router = require("./routes/index.js");
 // const router = require("./routes/index");
 const router = require("./routes/");
 app.use(router);
+/* -------------------------------------------------------------------------- */
 
-/* ------------------------------------------------------- */
+/* -------------------------------------------------------------------------- */
 app.listen(PORT, () => console.log("Running: http://127.0.0.1:" + PORT));
