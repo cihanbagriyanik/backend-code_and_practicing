f60b0d90ba255985f8f1c45bb70ecdd8c47b7b85
cihanbagriyanik <cihanbagriyanikde@gmail.com>
2024-01-31 11:27:48 +0100

    Routers_Middlewares 31.01.2024 middleware end

--- code/nodejs/10-Routers_Middlewares/middleware.js
+++ code/nodejs/10-Routers_Middlewares/middleware.js
@@ -10,10 +10,103 @@ require("dotenv").config();
 const PORT = process.env.PORT || 8000;
 
 /* -------------------------------------------------------------------------- */
-//? Middleware functions must be has three parameters. 
+//? Middleware functions must be has three parameters.
 //? Last parameter is for next().
 
+// // Middleware:
+// app.get("/", (req, res, next) => {
+//   //   res.send({
+//   //     message: "Middleware Running",
+//   //   });
+
+//   // Go to next function:
+//   next();
+// });
+
+// // Route-Path:
+// app.get("/", (req, res) => {
+//   res.send({
+//     message: "Welcome",
+//   });
+// });
+
 /* -------------------------------------------------------------------------- */
+// // Middleware:
+// app.get("/", (req, res, next) => {
+//   if (req.query.username == "clarusway") {
+//     // Send data with req/res:
+//     req.newVariable = "Welcome";
+//     res.username = "Clarusway";
+
+//     next();
+//   } else {
+//     res.send({
+//       message: "username is wrong!",
+//     });
+//   }
+// });
+
+// // Route-Path:
+// app.get("/", (req, res) => {
+//   res.send({
+//     message: req.newVariable + " " + res.username,
+//   });
+// });
+/* -------------------------------------------------------------------------- */
+// // Middleware:
+// app.get("/", (req, res, next) => {
+//   req.message1 = "Middleware-1 runned.";
+//   next();
+// });
+// app.get("/", (req, res, next) => {
+//   req.message2 = "Middleware-2 runned.";
+//   next();
+// });
+// app.get("/", (req, res, next) => {
+//   req.message3 = "Middleware-3 runned.";
+//   next();
+// });
+// app.get("/", (req, res, next) => {
+//   req.message4 = "Middleware-3 runned.";
+//   next();
+// });
+
+// // Route-Path:
+// app.get("/", (req, res) => {
+//   res.send({
+//     message1: req.message1,
+//     message2: req.message2,
+//     message3: req.message3,
+//     message4: req.message4,
+//     message: "Finished",
+//   });
+// });
+
+/* -------------------------------------------------------------------------- */
+
+const middleFunction1 = (req, res, next) => {
+  req.message1 = "MiddlewareFunction-1 runned.";
+  next();
+};
+const middleFunction2 = (req, res, next) => {
+  req.message2 = "MiddlewareFunction-2 runned.";
+  next();
+};
+
+// Call Middlewares:
+app.use("/", middleFunction1, middleFunction2); // Recommended
+// app.use("/", [middleFunction1, middleFunction2]);
+
+// Route-Path:
+app.get("/", (req, res) => {
+// app.get("/", middleFunction1, middleFunction2, (req, res) => { // middleware functions callback routing hemen oncesinde de cagirabilir.
+  res.send({
+    message1: req.message1,
+    message2: req.message2,
+
+    message: "Finished",
+  });
+});
 
 /* -------------------------------------------------------------------------- */
 app.listen(PORT, () => console.log("Running: http://127.0.0.1:" + PORT));
