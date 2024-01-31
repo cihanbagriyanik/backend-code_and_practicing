e5adcea210c9dcfaa67f42ec3d7bec6110e78725
cihanbagriyanik <cihanbagriyanikde@gmail.com>
2024-01-31 12:01:28 +0100

    Routers_Middlewares 31.01.2024 middleware function export import

--- code/nodejs/10-Routers_Middlewares/middleware.js
+++ code/nodejs/10-Routers_Middlewares/middleware.js
@@ -117,19 +117,30 @@ const PORT = process.env.PORT || 8000;
 // const middleFunction1 = require("./middlewares/")
 // const middleFunctionArray = require("./middlewares/");
 // const [middleFunction1, middleFunction2] = require("./middlewares/");
-const {middleFunction1, middleFunction2} = require("./middlewares/");
+const { middleFunction1, middleFunction2 } = require("./middlewares/");
 
-// app.get("/", middleFunction1, (req, res) => {
-// app.get("/", middleFunctionArray, (req, res) => {
-// app.get("/", middleFunction1, middleFunction2, (req, res) => {
-app.get("/", middleFunction1, middleFunction2, (req, res) => {
+app.use("/", middleFunction1, middleFunction2);
+// app.use("/path", middleFunction1, middleFunction2);
+// app.use(middleFunction1, middleFunction2); // default butun url ler yani " * "
+
+app.get("/", (req, res) => {
+  // app.get("/", middleFunction1, (req, res) => {
+  // app.get("/", middleFunctionArray, (req, res) => {
+  // app.get("/", middleFunction1, middleFunction2, (req, res) => {
+  // app.get("/", middleFunction1, middleFunction2, (req, res) => {
   res.send({
     message1: req.message1,
     message2: req.message2,
-
     message: "Finished",
   });
 });
 
+// app.get("/path", (req, res) => {
+//   res.send({
+//     message1: req.message1,
+//     message2: req.message2,
+//     message: "Path Finished",
+//   });
+// });
 /* -------------------------------------------------------------------------- */
 app.listen(PORT, () => console.log("Running: http://127.0.0.1:" + PORT));
