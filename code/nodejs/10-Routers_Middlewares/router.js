a503efb4985fe0ed51452b0a7bda5aee7ef31db0
cihanbagriyanik <cihanbagriyanikde@gmail.com>
2024-01-31 10:35:35 +0100

    Routers_Middlewares Start 31.01.2024

---
+++ code/nodejs/10-Routers_Middlewares/router.js
@@ -0,0 +1,20 @@
+"use strict";
+/* -------------------------------------------------------
+    EXPRESSJS - ROUTING
+------------------------------------------------------- */
+
+const express = require("express");
+const app = express();
+
+require("dotenv").config();
+const PORT = process.env.PORT || 8000;
+
+/* ------------------------------------------------------- */
+//? "Router" is special app for URL control in ExpressJS.
+
+// app.get('/', (req, res) => { res.send({ message: 'Home Page' }) })
+// app.get('/about', (req, res) => { res.send({ message: 'About Page' }) })
+// app.get('/user/:userId', (req, res) => { res.send({ message: 'User Page' })
+
+/* ------------------------------------------------------- */
+app.listen(PORT, () => console.log("Running: http://127.0.0.1:" + PORT));
