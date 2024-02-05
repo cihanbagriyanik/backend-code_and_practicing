dc54ccc734e16dd6f7b39d18465eb4003902f263
cihanbagriyanik <cihanbagriyanikde@gmail.com>
2024-02-05 11:16:15 +0100

    05.02.2024 12_Model_Todo_1 create db

--- code/nodejs/12-Models_Todo_1/index.js
+++ code/nodejs/12-Models_Todo_1/index.js
@@ -16,8 +16,69 @@ app.use(express.json());
 app.all("/", (req, res) => {
   res.send("WELCOME TO TODO API");
 });
+/* -------------------------------------------------------------------------- */
+// https://sequelize.org/docs/v6/getting-started/
 
-// continue from here...
+const { Sequelize, DataTypes } = require("sequelize");
+
+// DB Connection:
+// const sequelize = new Sequelize("sqlite:./db.sqlite3")
+const sequelize = new Sequelize(
+  "sqlite:" + process.env.SQLITE || "./db.sqlite3"
+);
+
+// PostgreSQL:
+// $ npm i pg pg-hstore
+// const sequelize = new Sequelize('postgres://user:pass@example.com:5432/dbname') // Example for postgres
+
+/* -------------------------------------------------------------------------- */
+// MODELS:
+// sequelize.define(' tableName ', { colums / fields })
+
+// https://sequelize.org/docs/v7/models/data-types/
+const Todo = sequelize.define("todos", {
+  //? NOT need define ID field, it will create auto.
+  //   id: {
+  type: DataTypes.INTEGER,
+  //     unique: true, // default: false
+  //     allowNull: false, // default: true
+  //     field: 'custom_column_name', // Change field name
+  //     comment: "description",
+  //     primaryKey: true, // default: false,
+  //     autoIncrement: true, // id++ // default: false
+  //     defaultValue: 0 // Set the value when if blank.
+  //   },
+
+  title: {
+    type: DataTypes.STRING(100), // VARCHAR( 100 )
+    allowNull: false,
+  },
+
+  description: DataTypes.TEXT, // ShortHand USING
+  //   description: {
+  //     type: DataTypes.TEXT,
+  //   },
+
+  priorty: {
+    // -1: low, 0: Normal, 1: High
+    type: DataTypes.TINYINT,
+    allowNull: false,
+    defaultValue: 0,
+  },
+
+  isDone: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+    defaultValue: false,
+  },
+
+  //? Not need define "createdAt" & "updatedAt" fields.
+  // createdAt: false, // Unset
+  // updatedAt: false, // Unset
+});
+
+// Synchronization:
+sequelize.sync(); // CREATE TABLE
 
 /* -------------------------------------------------------------------------- */
 const errorHandler = (err, req, res, next) => {
