62d0a3dc92d4817bff2ebd5ba74776f4db58354d
cihanbagriyanik <cihanbagriyanikde@gmail.com>
2024-02-12 10:12:26 +0100

    blog category controller end

--- code/nodejs/15-BlogApi_UserModel/src/controllers/blogController.js
+++ code/nodejs/15-BlogApi_UserModel/src/controllers/blogController.js
@@ -9,8 +9,67 @@ require("express-async-errors");
 
 const { BlogPost, BlogCategory } = require("../models/blogModel");
 
-//? BlogCategory Controller
+/* -------------------------------------------------------------------------- */
+//? Blog Category CONTROLLER
+module.exports.BlogCategory = {
+  list: async (req, res) => {
+    const data = await BlogCategory.find();
 
+    res.status(200).send({
+      error: false,
+      data: data,
+    });
+  },
+
+  // CRUD Processes:
+  // POST
+  create: async (req, res) => {
+    const data = await BlogCategory.create(req.body);
+
+    res.status(201).send({
+      error: false,
+      body: req.body,
+      data: data,
+    });
+  },
+
+  // /:categoryId -> GET
+  read: async (req, res) => {
+    const data = await BlogCategory.findOne({ _id: req.params.categoryId });
+
+    res.status(200).send({
+      error: false,
+      data: data,
+    });
+  },
+
+  // /:categoryId -> PUT / PATCH
+  update: async (req, res) => {
+    const data = await BlogCategory.updateOne(
+      { _id: req.params.categoryId },
+      req.body
+    );
+
+    res.status(202).send({
+      error: false,
+      // data: data,
+      data,
+      newData: await BlogCategory.findOne({ _id: req.params.categoryId }),
+    });
+  },
+
+  // /:categoryId -> DELETE
+  delete: async (req, res) => {
+    const data = await BlogCategory.deleteOne({ _id: req.params.categoryId });
+
+    res.status(data.deletedCount >= 1 ? 204 : 404).send({
+      error: false,
+      data,
+    });
+  },
+};
+
+/* -------------------------------------------------------------------------- */
 //? BLOG POST CONTROLLER
 module.exports.BlogPost = {
   list: async (req, res) => {
@@ -22,6 +81,7 @@ module.exports.BlogPost = {
     });
   },
 
+  // CRUD
   create: async (req, res) => {
     const data = await BlogPost.create(req.body);
 
