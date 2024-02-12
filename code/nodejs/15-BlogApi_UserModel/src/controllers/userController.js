2766c35adb7521c337960bb83724efc46ccf2ee3
cihanbagriyanik <cihanbagriyanikde@gmail.com>
2024-02-12 12:41:24 +0100

    password Encyrpt added

--- code/nodejs/15-BlogApi_UserModel/src/controllers/userController.js
+++ code/nodejs/15-BlogApi_UserModel/src/controllers/userController.js
@@ -43,7 +43,9 @@ module.exports.User = {
 
   // /:userId -> PUT / PATCH
   update: async (req, res) => {
-    const data = await User.updateOne({ _id: req.params.userId }, req.body);
+    const data = await User.updateOne({ _id: req.params.userId }, req.body, {
+      runValidators: true,
+    });
 
     res.status(202).send({
       error: false,
--- code/nodejs/15-BlogApi_UserModel/src/models/userModel.js
+++ code/nodejs/15-BlogApi_UserModel/src/models/userModel.js
@@ -5,6 +5,27 @@
 
 const mongoose = require("mongoose");
 
+/* -------------------------------------------------------------------------- */
+//? Password Encrypt:
+// // https://nodejs.org/api/crypto.html#cryptopbkdf2syncpassword-salt-iterations-keylen-digest
+// const crypto = require("node:crypto");
+
+// // const keyCode = "write_random_chars_in_here";
+// const keyCode = process.env.SECRET_KEY || "write_random_chars_in_here";
+// const loopCount = 10_000;
+// const charCount = 32; // write 32 for 64
+// const encType = "sha512";
+
+// const passwordEncrypt = function (password) {
+//   return crypto
+//     .pbkdf2Sync(password, keyCode, loopCount, charCount, encType)
+//     .toString("hex");
+// };
+
+const passwordEncrypt = require("../helpers/passwordEncrypt");
+
+/* -------------------------------------------------------------------------- */
+
 // Schema:
 const UserSchema = new mongoose.Schema(
   {
@@ -35,8 +56,13 @@ const UserSchema = new mongoose.Schema(
       type: String,
       trim: true,
       required: true,
+      set: (password) => {
+        passwordEncrypt(password);
+      },
     },
+
     firstName: String,
+
     lastName: String,
   },
 
