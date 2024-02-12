03c9f7bf5939c6f5e55fff8c19a708128dbb670d
cihanbagriyanik <cihanbagriyanikde@gmail.com>
2024-02-12 14:03:03 +0100

    BlogAppAPI Self Started

---
+++ self_practicing/BlogApi/index.js
@@ -0,0 +1,11 @@
+"use strict";
+
+/* -------------------------------------------------------------------------- //
+
+    BLOGAPP API Project INDEX.js
+
+/* -------------------------------------------------------------------------- */
+
+// Express Export
+const express = require("express");
+const app = express();
