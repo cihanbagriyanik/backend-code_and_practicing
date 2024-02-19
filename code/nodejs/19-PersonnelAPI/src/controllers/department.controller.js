d97ad305bd5204f75f155c743c67d8fbf040de8a
cihanbagriyanik <cihanbagriyanikde@gmail.com>
2024-02-19 11:43:00 +0100

    PersonnelAPI personnnel END

--- code/nodejs/19-PersonnelAPI/src/controllers/department.controller.js
+++ code/nodejs/19-PersonnelAPI/src/controllers/department.controller.js
@@ -1,82 +1,15 @@
-// "use strict";
-// /* --------------------------------------------------------------------------
-//     * EXPRESS - Personnel API
-// ----------------------------------------------------------------------------- */
-// /* -------------------------------------------------------------------------- */
-// //? Require(import) Department:
-// const { Department } = require("../models/department.model");
-// /* -------------------------------------------------------------------------- */
-// //? Department Controller:
-// module.exports = {
-//   // GET:
-//   list: async (req, res) => {
-//     const data = await res.getModelList(Department);
-
-//     res.status(200).send({
-//       error: false,
-//       detail: await res.getModelListDetails(Department),
-//       data, // data: data,
-//     });
-//   },
-
-//   // CRUD Processes:
-//   // POST
-//   creat: async (req, res) => {
-//     const data = await Department.create(req.body);
-
-//     res.status(201).send({
-//       error: false,
-//       data,
-//     });
-//   },
-
-//   // /:id -> GET
-//   read: async (req, res) => {
-//     const data = await Department.findOne({ _id: req.params.id });
-
-//     res.status(200).send({
-//       error: false,
-//       data,
-//     });
-//   },
-
-//   // /:id -> PUT / PATCH
-//   update: async (req, res) => {
-//     const data = await Department.updateOne({ _id: req.params.id }, req.body);
-
-//     res.status(202).send({
-//       error: false,
-//       data,
-//       newData: await Department.findOne({ _id: req.params.id }),
-//     });
-//   },
-
-//   // /:id -> DELETE
-//   delete: async (req, res) => {
-//     const data = await Department.deleteOne({ _id: req.params.id });
-
-//     res.status(data.deletedCount ? 204 : 404).send({
-//       error: !data.deletedCount,
-//       data,
-//     });
-
-//     // const isDeleted = data.deletedCount >= 1 ? true : false;
-//     // res.status(isDeleted ? 204 : 404).send({
-//     //   error: !isDeleted,
-//     //   data,
-//     // });
-//   },
-// };
-// /* -------------------------------------------------------------------------- */
-
 "use strict";
-/* -------------------------------------------------------
-    EXPRESS - Personnel API
-------------------------------------------------------- */
-
+/* --------------------------------------------------------------------------
+    * EXPRESS - Personnel API
+----------------------------------------------------------------------------- */
+/* -------------------------------------------------------------------------- */
+//? Require(import) Department:
 const Department = require("../models/department.model");
 
+/* -------------------------------------------------------------------------- */
+//? Department Controller:
 module.exports = {
+  // GET:
   list: async (req, res) => {
     // const data = await Department.find(search).sort(sort).skip(skip).limit(limit)
     const data = await res.getModelList(Department);
@@ -88,6 +21,8 @@ module.exports = {
     });
   },
 
+  // CRUD Processes:
+  // POST
   create: async (req, res) => {
     const data = await Department.create(req.body);
 
@@ -97,6 +32,7 @@ module.exports = {
     });
   },
 
+  // /:id -> GET
   read: async (req, res) => {
     const data = await Department.findOne({ _id: req.params.id });
 
@@ -106,16 +42,18 @@ module.exports = {
     });
   },
 
+  // /:id -> PUT / PATCH
   update: async (req, res) => {
     const data = await Department.updateOne({ _id: req.params.id }, req.body);
 
     res.status(202).send({
       error: false,
       data,
-      new: await Department.findOne({ _id: req.params.id }),
+      newData: await Department.findOne({ _id: req.params.id }),
     });
   },
 
+  // /:id -> DELETE
   delete: async (req, res) => {
     const data = await Department.deleteOne({ _id: req.params.id });
 
@@ -125,7 +63,6 @@ module.exports = {
     });
 
     // const isDeleted = data.deletedCount >= 1 ? true : false;
-
     // res.status(isDeleted ? 204 : 404).send({
     //   error: !isDeleted,
     //   data,
--- code/nodejs/19-PersonnelAPI/src/controllers/personnel.controller.js
+++ code/nodejs/19-PersonnelAPI/src/controllers/personnel.controller.js
@@ -1,4 +1,71 @@
-"use strict"
-/* -------------------------------------------------------
+"use strict";
+/* --------------------------------------------------------------------------
     * EXPRESS - Personnel API
-------------------------------------------------------- */
+----------------------------------------------------------------------------- */
+/* -------------------------------------------------------------------------- */
+// //? Require(import) Personnel:
+const Personnel = require("../models/personnel.model");
+
+/* -------------------------------------------------------------------------- */
+//? Personnel Controller:
+module.exports = {
+  // GET:
+  list: async (req, res) => {
+    // const data = await Personnel.find(search).sort(sort).skip(skip).limit(limit)
+    const data = await res.getModelList(Personnel);
+
+    res.status(200).send({
+      error: false,
+      detail: await res.getModelListDetails(Personnel),
+      data, // data: data
+    });
+  },
+
+  // CRUD Processes:
+  // POST
+  create: async (req, res) => {
+    const data = await Personnel.create(req.body);
+
+    res.status(201).send({
+      error: false,
+      data,
+    });
+  },
+
+  // /:id -> GET
+  read: async (req, res) => {
+    const data = await Personnel.findOne({ _id: req.params.id });
+
+    res.status(200).send({
+      error: false,
+      data,
+    });
+  },
+
+  // /:id -> PUT / PATCH
+  update: async (req, res) => {
+    const data = await Personnel.updateOne({ _id: req.params.id }, req.body);
+
+    res.status(202).send({
+      error: false,
+      data,
+      newData: await Personnel.findOne({ _id: req.params.id }),
+    });
+  },
+
+  // /:id -> DELETE
+  delete: async (req, res) => {
+    const data = await Personnel.deleteOne({ _id: req.params.id });
+
+    res.status(data.deletedCount ? 204 : 404).send({
+      error: !data.deletedCount,
+      data,
+    });
+
+    // const isDeleted = data.deletedCount >= 1 ? true : false;
+    // res.status(isDeleted ? 204 : 404).send({
+    //   error: !isDeleted,
+    //   data,
+    // });
+  },
+};
