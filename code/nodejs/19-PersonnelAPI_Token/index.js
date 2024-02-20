af636f2d2c3c7c4f2966dfde4806f3c8dcc432f0
cihanbagriyanik <cihanbagriyanikde@gmail.com>
2024-02-20 09:22:34 +0100

    PersonalAPI_Token start

---
+++ code/nodejs/19-PersonnelAPI_Token/index.js
@@ -0,0 +1,72 @@
+"use strict";
+/* --------------------------------------------------------------------------
+    * EXPRESS - Personnel API
+----------------------------------------------------------------------------- */
+/*
+    $ npm i express dotenv mongoose express-async-errors
+    $ npm i cookie-session
+    $ npm i jsonwebtoken
+*/
+
+const express = require("express");
+const app = express();
+
+require("dotenv").config();
+
+/* -------------------------------------------------------------------------- */
+// * Required Modules:
+//? envVariables to process.env:
+const PORT = process.env.PORT || 8000;
+
+//? asyncErrors to errorHandler:
+require("express-async-errors");
+
+/* -------------------------------------------------------------------------- */
+//? Configrations:
+const { dbConnection } = require("./src/configs/dbConnection");
+dbConnection();
+
+/* -------------------------------------------------------------------------- */
+//? Middlerware:
+// Accept JSON:
+app.use(express.json());
+
+// SessionsCookies:
+app.use(
+  require("cookie-session")({
+    secret: process.env.SECRET_KEY || "write_random_chars_in_here",
+  })
+);
+
+// res.getModelList():
+app.use(require("./src/middlewares/findSearchSortPage"));
+
+/* -------------------------------------------------------------------------- */
+//? Routes:
+// HomePath:
+app.all("/", (req, res) => {
+  res.send({
+    error: false,
+    message: "Welcome to PERSONNEL API", // INSIDE OBJE
+  });
+});
+
+//* /departments
+app.use("/departments", require("./src/routes/department.router"));
+
+//* /personnels
+app.use("/personnels", require("./src/routes/personnel.router"));
+
+/* -------------------------------------------------------------------------- */
+
+/* -------------------------------------------------------------------------- */
+//? errorHandler:
+app.use(require("./src/middlewares/errorHandler"));
+
+/* -------------------------------------------------------------------------- */
+//? RUN SERVER:
+app.listen(PORT, () => console.log("http://127.0.0.1:" + PORT));
+
+/* -------------------------------------------------------------------------- */
+//? Syncronization (must be in commentLine):
+// require('./src/helpers/sync')()
