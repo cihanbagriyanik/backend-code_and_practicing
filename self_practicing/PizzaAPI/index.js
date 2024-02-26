90578d60dc8288ea3099db2b8dabf791772b199c
cihanbagriyanik <cihanbagriyanikde@gmail.com>
2024-02-26 15:43:08 +0100

    seld PizzaAPI topings end

--- self_practicing/PizzaAPI/index.js
+++ self_practicing/PizzaAPI/index.js
@@ -1,7 +1,7 @@
-"use strict"
-/* -------------------------------------------------------
-    NODEJS EXPRESS | CLARUSWAY FullStack Team
-------------------------------------------------------- */
+"use strict";
+/* --------------------------------------------------------------------------
+    * NODEJS EXPRESS | CLARUSWAY FullStack Team
+----------------------------------------------------------------------------- */
 /*
     $ cp .env-sample .env
     $ npm init -y
@@ -9,44 +9,74 @@
     $ mkdir logs
     $ nodemon
 */
-const express = require('express')
-const app = express()
-
-/* ------------------------------------------------------- */
-// Required Modules:
+/* -------------------------------------------------------------------------- */
+//? Required Modules:
+const express = require("express");
+const app = express();
 
 // envVariables to process.env:
-require('dotenv').config()
-const PORT = process.env?.PORT || 8000
+require("dotenv").config();
+const PORT = process.env?.PORT || 8000;
 
 // asyncErrors to errorHandler:
-require('express-async-errors')
+require("express-async-errors");
 
-/* ------------------------------------------------------- */
-// Configrations:
+/* -------------------------------------------------------------------------- */
+//? Configrations:
 
 // Connect to DB:
-const { dbConnection } = require('./src/configs/dbConnection')
-dbConnection()
+const { dbConnection } = require("./src/configs/dbConnection");
+dbConnection();
 
-/* ------------------------------------------------------- */
-// Middlewares:
+/* -------------------------------------------------------------------------- */
+//? Middlewares:
 
+// Accept JSON:
+app.use(express.json());
 
+// Check Token:
+app.use(require("./src/middlewares/authentication"));
 
-/* ------------------------------------------------------- */
-// Routes:
+// morgan-logger:
+app.use(require("./src/middlewares/logger"));
 
+// res.getModelList:
+app.use(require("./src/middlewares/findSearchSortPage"));
 
+/* -------------------------------------------------------------------------- */
+//? Routes:
 
-/* ------------------------------------------------------- */
+// HomePath:
+app.all("/", (req, res) => {
+  res.send({
+    error: false,
+    message: "Welcome to PIZZA API",
+    isLogin: req.isLogin,
+    user: req.user,
+  });
+});
 
-// errorHandler:
-app.use(require('./src/middlewares/errorHandler'))
+// auth:
+app.use("/auth", require("./src/routes/auth"));
+// user:
+app.use("/users", require("./src/routes/user"));
+// token:
+app.use("/tokens", require("./src/routes/token"));
+// order:
+app.use("/orders", require("./src/routes/order"));
+// pizza:
+app.use("/pizzas", require("./src/routes/pizza"));
+// topping:
+app.use("/toppings", require("./src/routes/topping"));
 
-// RUN SERVER:
-app.listen(PORT, () => console.log('http://127.0.0.1:' + PORT))
+/* -------------------------------------------------------------------------- */
+//? errorHandler:
+app.use(require("./src/middlewares/errorHandler"));
 
-/* ------------------------------------------------------- */
-// Syncronization (must be in commentLine):
+/* -------------------------------------------------------------------------- */
+//? RUN SERVER:
+app.listen(PORT, () => console.log("http://127.0.0.1:" + PORT));
+
+/* -------------------------------------------------------------------------- */
+//? Syncronization (must be in commentLine):
 // require('./src/helpers/sync')() // !!! It clear database.
