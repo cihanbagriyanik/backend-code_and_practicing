bfe746615beb1541e5d51d023083bbaa72a60d71
cihanbagriyanik <cihanbagriyanikde@gmail.com>
2024-03-06 12:35:28 +0100

    cars and reservation end

--- code/nodejs/27-RentACar/src/routes/car.js
+++ code/nodejs/27-RentACar/src/routes/car.js
@@ -1,7 +1,24 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
-const router = require('express').Router()
+const router = require("express").Router();
 /* ------------------------------------------------------- */
 // routes/car:
+
+const permissions = require("../middlewares/permissions");
+const car = require("../controllers/car");
+
+// URL: /cars
+
+router.route("/").get(car.list).post(permissions.isStaffOrisAdmin, car.create);
+
+router
+  .route("/:id")
+  .get(car.read)
+  .put(permissions.isStaffOrisAdmin, car.update)
+  .patch(permissions.isStaffOrisAdmin, car.update)
+  .delete(permissions.isStaffOrisAdmin, car.delete);
+
+/* ------------------------------------------------------- */
+module.exports = router;
--- code/nodejs/27-RentACar/src/routes/reservation.js
+++ code/nodejs/27-RentACar/src/routes/reservation.js
@@ -1,7 +1,26 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
-const router = require('express').Router()
+const router = require("express").Router();
 /* ------------------------------------------------------- */
-// routes/reservation:
+// routes/car:
+
+const reservation = require("../controllers/reservation");
+
+const { isLogin } = require("../middlewares/permissions");
+router.use(isLogin);
+
+// URL: /reservations
+
+router.route("/").get(reservation.list).post(reservation.create);
+
+router
+  .route("/:id")
+  .get(reservation.read)
+  .put(reservation.update)
+  .patch(reservation.update)
+  .delete(reservation.delete);
+
+/* ------------------------------------------------------- */
+module.exports = router;
