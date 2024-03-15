ef844d4a6e0b419ce6c212216071e515d6a171af
cihanbagriyanik <cihanbagriyanikde@gmail.com>
2024-03-15 09:58:36 +0100

    docker 2 start

---
+++ code/nodejs/34-Docker_2/BE-StockAPI/dockerfile
@@ -0,0 +1,11 @@
+FROM node:alpine3.18
+
+WORKDIR /backend
+
+COPY . . 
+
+RUN npm install --global nodemon
+
+RUN npm install 
+
+CMD npm start
---
+++ code/nodejs/34-Docker_2/FE-StockAPI/src/pages/Home.jsx
@@ -0,0 +1,22 @@
+import { Container } from '@mui/material'
+import React, { useEffect } from 'react'
+import PageHeader from '../components/PageHeader'
+import KpiCards from '../components/KpiCards'
+import Charts from '../components/Charts'
+import useStockCall from '../hooks/useStockCall'
+
+const Home = () => {
+  const {getPurSales} = useStockCall()
+  useEffect(()=>{
+    getPurSales()
+  },[])
+  return (
+    <Container maxWidth="xl">
+      <PageHeader text="Dashboard"/>
+      <KpiCards/>
+      <Charts/>
+    </Container>
+  )
+}
+
+export default Home
