c8cecef4d91ca1f920ba53c8cccfd01fd9ce42d7
cihanbagriyanik <cihanbagriyanikde@gmail.com>
2024-03-20 10:18:08 +0100

    blogapp ejs 1.class end

---
+++ code/nodejs/35-BlogApp-Ejs-1/inclass/src/controllers/blogControllerView.js
@@ -0,0 +1,133 @@
+"use strict";
+/*
+
+BLOG api project MODELS
+
+*/
+// move to index.js:
+// require('express-async-errors')
+
+const { BlogCategory, BlogPost } = require("../models/blogModel");
+
+/*
+BLOG POST Controllers
+*/
+
+module.exports.BlogPost = {
+  list: async (req, res) => {
+    const data = await res.getModelList(BlogPost, "blogCategoryId");
+
+    // res.status(200).send({
+    //   error: false,
+    //   details: await res.getModelListDetails(BlogPost),
+    //   data: data,
+    // });
+
+    res.render("index");
+  },
+
+  create: async (req, res) => {
+    //
+    const data = await BlogPost.create(req.body);
+    // console.log('*****');
+    // console.log(req.body);
+    res.status(200).send({
+      error: false,
+      body: req.body,
+      data: data,
+    });
+  },
+  read: async (req, res) => {
+    //
+    const data = await BlogPost.findOne({ _id: req.params.postId }).populate(
+      "blogCategoryId"
+    );
+    res.status(200).send({
+      error: false,
+      data: data,
+    });
+  },
+  update: async (req, res) => {
+    //
+    const data = await BlogPost.updateOne({ _id: req.params.postId }, req.body);
+
+    res.status(200).send({
+      error: false,
+      data: data,
+    });
+  },
+  delete: async (req, res) => {
+    //
+    const data = await BlogPost.deleteOne({ _id: req.params.postId });
+    // console.log(data);
+    res.status(data.deletedCount >= 1 ? 204 : 404).send({
+      error: false,
+    });
+  },
+};
+
+/*
+    BLOG CATEGORY Controllers
+    */
+
+module.exports.BlogCategory = {
+  // GET
+  list: async (req, res) => {
+    // const data = await BlogCategory.find()
+    const data = await res.getModelList(BlogCategory);
+
+    res.status(200).send({
+      error: false,
+      data,
+    });
+  },
+
+  // CRUD Processes:
+
+  // POST
+  create: async (req, res) => {
+    const data = await BlogCategory.create(req.body);
+
+    res.status(201).send({
+      error: false,
+      body: req.body,
+      data,
+    });
+  },
+
+  // /:categoryId -> GET
+  read: async (req, res) => {
+    const data = await BlogCategory.findOne({ _id: req.params.categoryId });
+
+    res.status(200).send({
+      error: false,
+      data,
+    });
+  },
+
+  // /:categoryId -> PUT / PATCH
+  update: async (req, res) => {
+    const data = await BlogCategory.updateOne(
+      { _id: req.params.categoryId },
+      req.body
+    );
+
+    res.status(202).send({
+      error: false,
+      body: req.body,
+      // data: data,
+      data,
+      newData: await BlogCategory.findOne({ _id: req.params.categoryId }),
+    });
+  },
+
+  // /:categoryId -> DELETE
+  delete: async (req, res) => {
+    const data = await BlogCategory.deleteOne({ _id: req.params.categoryId });
+
+    res.status(data.deletedCount >= 1 ? 204 : 404).send({
+      error: false,
+      data,
+    });
+  },
+};
---
+++ code/nodejs/35-BlogApp-Ejs-1/inclass/src/routes/views.js
@@ -0,0 +1,31 @@
+"use strict";
+/*
+
+BLOG Template project ROUTES
+
+*/
+const router = require("express").Router();
+const {
+  BlogCategory: blogCategoryView,
+  BlogPost: blogPostView,
+} = require("../controllers/blogControllerView");
+
+//? Browserlardan sadce ve sadece GET isteği gelir. HTML Form aracılığıyla sadece GET ve POST isteği gelir. O nedenle route planlaması yaparken diğer istekler için ona uygun bir planlama yapılmalıdır.
+
+//* method olarak get ve post diye de kullanılabilir. Yada all metodu kullanılarak controllerda gelen istek kontrol edilebilir.
+
+// URL: /
+//* / anasayfayı viewe verdiğimiz için projemiz ayaga kalkdığında hatalı sayfa gelecektiir çünkü routeu bulamayacaktır. Çünkü /post şeklinde ayarlama yaptım. Bunun sebebi de ilerleyen süreçlerde yeni routelar eklenebilir diye. Bunun i,çin ana sayfay istek geldiğinde /post a yönlendirme yaptırabilirim:
+
+router.all("/", (req, res) => {
+  res.redirect("/post");
+});
+
+// BlogPost:
+router.all("/post", blogPostView.list);
+router.all("/post/create", blogPostView.create);
+router.all("/post/:postId", blogPostView.read);
+router.all("/post/:postId/update", blogPostView.update);
+router.all("/post/:postId/delete", blogPostView.delete);
+
+module.exports = router;
