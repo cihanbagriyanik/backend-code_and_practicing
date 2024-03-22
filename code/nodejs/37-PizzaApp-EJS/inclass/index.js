69ad2728dc19a22cfde1195ac1a4fd9a228e3c2b
cihanbagriyanik <cihanbagriyanikde@gmail.com>
2024-03-22 09:56:38 +0100

    set Delimiter

--- code/nodejs/37-PizzaApp-EJS/inclass/index.js
+++ code/nodejs/37-PizzaApp-EJS/inclass/index.js
@@ -1,4 +1,4 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
@@ -12,116 +12,124 @@
     $ mkdir logs
     $ nodemon
 */
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
 /* ------------------------------------------------------- */
 // Required Modules:
 
 // envVariables to process.env:
-require('dotenv').config()
-const PORT = process.env?.PORT || 8000
+require("dotenv").config();
+const PORT = process.env?.PORT || 8000;
 
 // asyncErrors to errorHandler:
-require('express-async-errors')
+require("express-async-errors");
 
 /* ------------------------------------------------------- */
 // TEMPLATE:
 
-app.set('views', './public')
-app.set('view engine', 'ejs')
+app.set("views", "./public");
+app.set("view engine", "ejs");
 
+// let ejs = require("ejs");
+// // < > &
+// // ejs.delimiter = '?'; //* % yerein soru israte
+// ejs.openDelimiter = "{"; //* < yerine { kullan
+// ejs.closeDelimiter = "}"; //* > yerine } kullan
 
+app.set("view options", {
+  openDelimiter: "{",
+  closeDelimiter: "}",
+});
+
+/* -------------------------------------------------------------------------- */
 // Accept form data & convert to object:
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 
 // StaticFiles:
-app.use('/assets', express.static('./public/assets'))
+app.use("/assets", express.static("./public/assets"));
 /* ------------------------------------------------------- */
 //* Template içi cookie-session ekledik
 // SessionCookies:
-const session = require("cookie-session")
-app.use(session({ secret: process.env.SECRET_KEY }))
+const session = require("cookie-session");
+app.use(session({ secret: process.env.SECRET_KEY }));
 
 /* ------------------------------------------------------- */
 // Configrations:
 
 // Connect to DB:
-const { dbConnection } = require('./src/configs/dbConnection')
-dbConnection()
-
-
+const { dbConnection } = require("./src/configs/dbConnection");
+dbConnection();
 
 /* ------------------------------------------------------- */
 // Middlewares:
 
 // Accept JSON:
-app.use(express.json())
+app.use(express.json());
 
 // Check Token:
-app.use(require('./src/middlewares/authentication'))
+app.use(require("./src/middlewares/authentication"));
 
 // morgan-logger:
-app.use(require('./src/middlewares/logger'))
+app.use(require("./src/middlewares/logger"));
 
 // res.getModelList:
-app.use(require('./src/middlewares/findSearchSortPage'))
-
+app.use(require("./src/middlewares/findSearchSortPage"));
 
 /* ------------------------------------------------------- */
 // Routes: TEMPLATE + SESSION:
 
 // HomePath:
-app.all('/', (req, res) => {
-    res.redirect('/pizzas')
-})
+app.all("/", (req, res) => {
+  res.redirect("/pizzas");
+});
 // auth:
-app.use('/auth', require('./src/routes/view/auth'))
+app.use("/auth", require("./src/routes/view/auth"));
 // user:
-app.use('/users', require('./src/routes/view/user'))
+app.use("/users", require("./src/routes/view/user"));
 // order:
-app.use('/orders', require('./src/routes/view/order'))
+app.use("/orders", require("./src/routes/view/order"));
 // pizza:
-app.use('/pizzas', require('./src/routes/view/pizza'))
+app.use("/pizzas", require("./src/routes/view/pizza"));
 // topping:
-app.use('/toppings', require('./src/routes/view/topping'))
+app.use("/toppings", require("./src/routes/view/topping"));
 
 /* ------------------------------------------------------- */
 // Routes: API + JWT:
 
 // HomePath:
-app.all('/api', (req, res) => {
-    res.send({
-        error: false,
-        message: 'Welcome to PIZZA API',
-        isLogin: req.isLogin,
-        user: req.user
-    })
-})
+app.all("/api", (req, res) => {
+  res.send({
+    error: false,
+    message: "Welcome to PIZZA API",
+    isLogin: req.isLogin,
+    user: req.user,
+  });
+});
 // auth:
-app.use('/api/auth', require('./src/routes/api/auth'))
+app.use("/api/auth", require("./src/routes/api/auth"));
 // user:
-app.use('/api/users', require('./src/routes/api/user'))
+app.use("/api/users", require("./src/routes/api/user"));
 // order:
-app.use('/api/orders', require('./src/routes/api/order'))
+app.use("/api/orders", require("./src/routes/api/order"));
 // pizza:
-app.use('/api/pizzas', require('./src/routes/api/pizza'))
+app.use("/api/pizzas", require("./src/routes/api/pizza"));
 // topping:
-app.use('/api/toppings', require('./src/routes/api/topping'))
+app.use("/api/toppings", require("./src/routes/api/topping"));
 
 // document:
-app.use('/api/documents', require('./src/routes/api/document'))
+app.use("/api/documents", require("./src/routes/api/document"));
 
 // static-files:
-app.use('/images', express.static('./uploads'))
+app.use("/images", express.static("./uploads"));
 
 /* ------------------------------------------------------- */
 
 // errorHandler:
-app.use(require('./src/middlewares/errorHandler'))
+app.use(require("./src/middlewares/errorHandler"));
 
 // RUN SERVER:
-app.listen(PORT, () => console.log('http://127.0.0.1:' + PORT))
+app.listen(PORT, () => console.log("http://127.0.0.1:" + PORT));
 
 /* ------------------------------------------------------- */
 // Syncronization (must be in commentLine):
