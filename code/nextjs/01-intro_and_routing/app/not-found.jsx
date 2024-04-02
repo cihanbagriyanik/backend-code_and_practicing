07b6979211f233f2fabfcc0014efa5d597fca473
cihanbagriyanik <cihanbagriyanikde@gmail.com>
2024-04-02 12:06:29 +0200

    sidebar and not found go back

--- code/nextjs/01-intro_and_routing/app/not-found.jsx
+++ code/nextjs/01-intro_and_routing/app/not-found.jsx
@@ -1,6 +1,10 @@
+"use client";
 import React from "react";
+import { useRouter } from "next/navigation";
 
+//? next.js olmayan route'lar deafult 404 sayfası sunmaktadır. Default 404 sayfasını "not-found.js" ile override edebiliriz
 const NotFound = () => {
+  const router = useRouter();
   return (
     <main>
       <div className="max-w-screen-xl mx-auto px-4 flex items-center justify-start h-screen md:px-8 ">
@@ -14,10 +18,16 @@ const NotFound = () => {
             removed.
           </p>
           <div className="flex flex-wrap items-center justify-center gap-3">
-            <button className="block py-2 px-4 hover:text-white font-medium bg-main duration-150 hover:bg-orange-600 active:bg-indigo-700 rounded-lg">
+            <button
+              onClick={() => router.back()}
+              className="block py-2 px-4 hover:text-white font-medium bg-main duration-150 hover:bg-orange-600 active:bg-indigo-700 rounded-lg"
+            >
               Go Back
             </button>
-            <button className="block py-2 px-4 text-gray-700 hover:bg-black  hover:text-white font-medium duration-150 active:bg-gray-100 border rounded-lg">
+            <button
+              onClick={() => router.push("/")}
+              className="block py-2 px-4 text-gray-700 hover:bg-black  hover:text-white font-medium duration-150 active:bg-gray-100 border rounded-lg"
+            >
               Go Home
             </button>
           </div>
