24868d14e05bb0987503e8ab00dd3fa61122f385
cihanbagriyanik <cihanbagriyanikde@gmail.com>
2024-04-05 09:32:40 +0200

    starter woth img and icons

--- code/nextjs/03-netflix-app/tailwind.config.js
+++ code/nextjs/03-netflix-app/tailwind.config.js
@@ -14,5 +14,16 @@ module.exports = {
       },
     },
   },
+
+  theme: {
+    extend: {
+      colors: {
+        "gray-dark-main": "#23242a",
+        "gray-dark-second": "#28292d",
+        "gray-light": "#d3dce6",
+        "red-main": "#ff4b45",
+      },
+    },
+  },
   plugins: [],
 };
