c689aac7e6ec26dc777f66baa93d302708ef4556
cihanbagriyanik <cihanbagriyanikde@gmail.com>
2024-04-05 09:52:00 +0200

    not-foung and home page end

--- code/nextjs/03-netflix-app/app/page.js
+++ code/nextjs/03-netflix-app/app/page.js
@@ -1,9 +1,19 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="">
-      <h1 className="text-white">Hello Netflix</h1>
+    <div className="relative h-screen w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
+      <div className="bg-black w-full h-full bg-opacity-50">
+        <div className="text-white text-center relative top-2/4 m-auto">
+          <h1 className="text-5xl font-[900] text-center mb-3">
+            Unlimited movies, TV shows, and more
+          </h1>
+          <p className="text-2xl font-[400]">Watch anywhere. Cancel anytime.</p>
+          <Link href="register">
+            <button className="btn-danger w-[250px]">Get Started</button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
