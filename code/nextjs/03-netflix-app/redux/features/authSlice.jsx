a95907fa2b7e211c3bf7a97e86a4ca6f26fdefdf
cihanbagriyanik <cihanbagriyanikde@gmail.com>
2024-04-05 11:19:06 +0200

    redux store authSlice and providers added

---
+++ code/nextjs/03-netflix-app/redux/features/authSlice.jsx
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  currentUser: "",
+  error: false,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    loginSuccess: (state, { payload }) => {
+      state.currentUser = payload;
+    },
+    logoutSuccess: (state) => (state.currentUser = ""),
+    fetchFail: (state) => (state.error = true),
+  },
+});
+
+export const { loginSuccess, logoutSuccess, fetchFail } = authSlice.actions;
+
+export default authSlice.reducer;
